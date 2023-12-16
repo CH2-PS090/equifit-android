f9651e56d2c9c06b91fb21678b86713f81fc6942
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2023-12-17 04:22:27 +0700

    Base Screen

--- app/src/main/java/com/ch2ps090/equifit/ui/screen/camera/CameraScreen.kt
+++ app/src/main/java/com/ch2ps090/equifit/ui/screen/camera/CameraScreen.kt
@@ -1,22 +1,58 @@
 package com.ch2ps090.equifit.ui.screen.camera
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.ch2ps090.equifit.R
+import com.ch2ps090.equifit.theme.Dark2
+import com.ch2ps090.equifit.theme.Primary
+import com.ch2ps090.equifit.theme.White
+import com.ch2ps090.equifit.theme.titleLargeIntegralRegular
 
 @Composable
 fun CameraScreen(
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier = modifier
+            .background(Dark2)
+            .fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Text(stringResource(R.string.menu_camera))
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxWidth(),
+        ) {
+            Text(
+                text = stringResource(R.string.menu_camera),
+                style = titleLargeIntegralRegular,
+                color = White
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = "Screen",
+                style = titleLargeIntegralRegular,
+                color = Primary
+            )
+        }
     }
+}
+
+@Preview
+@Composable
+fun CameraScreenPreview() {
+    CameraScreen()
 }
